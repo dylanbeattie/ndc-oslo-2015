3f1e476c2734440be4ef5e073f24f237f60d5f7d
Dylan Beattie <dylan@dylanbeattie.net>
2015-06-14 15:26:39 +0100

    Added bundle transform syntax for SASS

---
+++ SassDemo/SassDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Optimization;
+using BundleTransformer.Core.Builders;
+using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Transformers;
+
+namespace SassDemo {
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles) {
+            var nullBuilder = new NullBuilder();
+            var styleTransformer = new StyleTransformer();
+            var scriptTransformer = new ScriptTransformer();
+            var nullOrderer = new NullOrderer();
+
+            var commonStylesBundle = new Bundle("~/Bundles/Styles");
+            commonStylesBundle.Include(
+                //               "~/Content/Fonts.css",
+                //               "~/Content/Site.css",
+                //               "~/Content/BundleTransformer.css",
+                //               "~/AlternativeContent/css/TestCssComponentsPaths.css",
+                //               "~/Content/themes/base/jquery.ui.core.css",
+                //               "~/Content/themes/base/jquery.ui.theme.css",
+                //               "~/Content/themes/base/jquery.ui.resizable.css",
+                //               "~/Content/themes/base/jquery.ui.button.css",
+                //               "~/Content/themes/base/jquery.ui.dialog.css",
+                //               "~/Content/TestTranslators.css",
+                //               "~/Content/less/TestLess.less",
+                //               "~/Content/sass/TestSass.sass",
+                "~/Content/Styles/Styles.scss");
+            commonStylesBundle.Builder = nullBuilder;
+            commonStylesBundle.Transforms.Add(styleTransformer);
+            commonStylesBundle.Orderer = nullOrderer;
+            bundles.Add(commonStylesBundle);
+
+        }
+    }
+}
--- SassDemo/SassDemo/Global.asax.cs
+++ SassDemo/SassDemo/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace SassDemo {
@@ -16,6 +17,8 @@ namespace SassDemo {
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
